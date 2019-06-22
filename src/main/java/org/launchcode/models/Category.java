95332f3df243a027c6491a712bda8306213faaf1
Justus Peterson-Rhodes <justus.rho4@gmail.com>
2019-06-22 16:11:43 -0500

    Part 1 Done, new files.

---
+++ src/main/java/org/launchcode/models/Category.java
@@ -0,0 +1,37 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Category {
+
+    @NotNull
+    @Size(min=3, max=15)
+    private String name;
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public Category() {}
+    
+    public Category(String newName) {
+        this.name = newName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
